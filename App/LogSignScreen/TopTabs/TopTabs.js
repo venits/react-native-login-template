9ae4370faa516786ae0a29a87e175a6d7f0d1719
Tomasz Przybył <31382349+venits@users.noreply.github.com>
2017-11-30 18:08:18 +0100

    Update TopTabs.js

--- App/LogSignScreen/TopTabs/TopTabs.js
+++ App/LogSignScreen/TopTabs/TopTabs.js
@@ -7,26 +7,28 @@ export default class TopTabs extends Component {
   constructor() {
     super();
     this.state = {
-      currentTabIndex: 0,
+      currentTabIndex: 0, // eslint-disable-line
       tabsStyles: [GLOBAL.topTabsStyle.topTabButtonOn, GLOBAL.topTabsStyle.topTabButtonOff],
     };
   }
 
-  changeTab = index => () => {
-    if (this.state.currentTabIndex !== index) {
-      this.props.switch(index);
-      this.state.tabsStyles.reverse();
-      this.setState({ currentTabIndex: index });
-    }
-  };
-
   render() {
     return (
       <View animation="fadeInLeft" delay={750} duration={700} style={GLOBAL.topTabsStyle.view}>
-        <Button full activeOpacity={1} style={this.state.tabsStyles[0]} onPress={this.changeTab(0)}>
+        <Button
+          full
+          activeOpacity={1}
+          style={this.state.tabsStyles[0]}
+          onPress={this.props.switch(0)}
+        >
           <Text style={GLOBAL.topTabsStyle.text} uppercase={false}>{language.loginTab}</Text>
         </Button>
-        <Button full activeOpacity={1} style={this.state.tabsStyles[1]} onPress={this.changeTab(1)}>
+        <Button
+          full
+          activeOpacity={1}
+          style={this.state.tabsStyles[1]}
+          onPress={this.props.switch(1)}
+        >
           <Text style={GLOBAL.topTabsStyle.text} uppercase={false}>{language.newAccount}</Text>
         </Button>
       </View>
